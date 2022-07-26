e030dbafe14ebf25d249c252f97852dc99b8f94e
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-26 22:57:45 +0530

    Connected to SQL Server

--- SQLConncetion.cs
+++ SQLConncetion.cs
@@ -14,41 +14,68 @@ namespace SQLConncetionADO
         public void Conncetion()
         {
             string cs = "Data Source=DESKTOP-C3BQOGP\\SQLEXPRESS;Initial Catalog=SQLConnction;Integrated Security = true;";
-            SqlConnection con = new SqlConnection(cs);
-            try
-            {
-               con.Open();
-               if (con.State == ConnectionState.Open)
-               {
-                   Console.WriteLine("SQL Connection has beem successfull:");
-                   Console.WriteLine("''''''''''''''''''''''''''''''''''''");
-                   Console.WriteLine("SqlConnection Information:");
-                   Console.WriteLine("  Connection State = " + con.State);
-                   Console.WriteLine("  Connection String = " + con.ConnectionString);
-                   Console.WriteLine("  Database Source = " + con.DataSource);
-                   Console.WriteLine("  Database = " + con.Database);
-                   Console.WriteLine("  Server Version = " + con.ServerVersion);
-                   Console.WriteLine("  Workstation Id = " + con.WorkstationId);
-                   Console.WriteLine("  Timeout = " + con.ConnectionTimeout);
-                   Console.WriteLine("  Packet Size = " + con.PacketSize);
-                   Console.ReadLine();
-               }
-               else
-               {
-                  Console.WriteLine("SqlConnection failed to open.");
-                  Console.WriteLine("  Connection State = " + con.State);
+            // using is useed for automatically close the connection
+            using(SqlConnection con = new SqlConnection(cs))
 
-               }
+            {
+                con.Open();
+                if (con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("SQL Connection has beem successfull:");
+                    Console.WriteLine("''''''''''''''''''''''''''''''''''''");
+                    Console.WriteLine("SqlConnection Information:");
+                    Console.WriteLine("  Connection State = " + con.State);
+                    Console.WriteLine("  Connection String = " + con.ConnectionString);
+                    Console.WriteLine("  Database Source = " + con.DataSource);
+                    Console.WriteLine("  Database = " + con.Database);
+                    Console.WriteLine("  Server Version = " + con.ServerVersion);
+                    Console.WriteLine("  Workstation Id = " + con.WorkstationId);
+                    Console.WriteLine("  Timeout = " + con.ConnectionTimeout);
+                    Console.WriteLine("  Packet Size = " + con.PacketSize);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("SqlConnection failed to open.");
+                    Console.WriteLine("  Connection State = " + con.State);
+
+                }
             }
-               catch(SqlException ex)
-               {
-                 Console.WriteLine(ex.Message);
-               }
-              finally
-              {
-                con.Close();
-              }
-                   
+
+            //try
+            //{
+
+                //    if (con.State == ConnectionState.Open)
+                //    {
+                //        Console.WriteLine("SQL Connection has beem successfull:");
+                //        Console.WriteLine("''''''''''''''''''''''''''''''''''''");
+                //        Console.WriteLine("SqlConnection Information:");
+                //        Console.WriteLine("  Connection State = " + con.State);
+                //        Console.WriteLine("  Connection String = " + con.ConnectionString);
+                //        Console.WriteLine("  Database Source = " + con.DataSource);
+                //        Console.WriteLine("  Database = " + con.Database);
+                //        Console.WriteLine("  Server Version = " + con.ServerVersion);
+                //        Console.WriteLine("  Workstation Id = " + con.WorkstationId);
+                //        Console.WriteLine("  Timeout = " + con.ConnectionTimeout);
+                //        Console.WriteLine("  Packet Size = " + con.PacketSize);
+                //        Console.ReadLine();
+                //    }
+                //else
+                //{
+                //  Console.WriteLine("SqlConnection failed to open.");
+                //  Console.WriteLine("  Connection State = " + con.State);
+
+                //}
+            //}
+            //   catch(SqlException ex)
+            //   {
+            //     Console.WriteLine(ex.Message);
+            //   }
+            //  finally
+            //  {
+            //    con.Close();
+            //  }
+
 
         }        
     }
