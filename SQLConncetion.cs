57129d0a963bf6856590c68f9e3e4e1ad3873515
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-27 23:06:10 +0530

    Inserted data into table

--- SQLConncetion.cs
+++ SQLConncetion.cs
@@ -1,56 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient; // SqlConnection class is present under this namespace
-using System.Data;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Data.SqlClient; // SqlConnection class is present under this namespace
+//using System.Data;
 
-namespace SQLConncetionADO
-{
-    public class SQLConncetion
-    {
+//namespace SQLConncetionADO
+//{
+//    public class SQLConncetion
+//    {
 
-        public void Conncetion()
-        {
-            string cs = "Data Source=DESKTOP-C3BQOGP\\SQLEXPRESS;Initial Catalog=SQLConnction;Integrated Security = true;";
+//        public void Conncetion()
+//        {
+//            string cs = "Data Source=DESKTOP-C3BQOGP\\SQLEXPRESS;Initial Catalog=SQLConnction;Integrated Security = true;";
 
-            SqlConnection con = new SqlConnection(cs);
-            try
-            {
-               con.Open();
-               if (con.State == ConnectionState.Open)
-               {
-                   Console.WriteLine("SQL Connection has beem successfull:");
-                   Console.WriteLine("''''''''''''''''''''''''''''''''''''");
-                   Console.WriteLine("SqlConnection Information:");
-                   Console.WriteLine("  Connection State = " + con.State);
-                   Console.WriteLine("  Connection String = " + con.ConnectionString);
-                   Console.WriteLine("  Database Source = " + con.DataSource);
-                   Console.WriteLine("  Database = " + con.Database);
-                   Console.WriteLine("  Server Version = " + con.ServerVersion);
-                   Console.WriteLine("  Workstation Id = " + con.WorkstationId);
-                   Console.WriteLine("  Timeout = " + con.ConnectionTimeout);
-                   Console.WriteLine("  Packet Size = " + con.PacketSize);
-                   Console.ReadLine();
-               }
-               else
-               {
-                  Console.WriteLine("SqlConnection failed to open.");
-                  Console.WriteLine("  Connection State = " + con.State);
+//            SqlConnection con = new SqlConnection(cs);
+//            try
+//            {
+//                con.Open();
+//                if (con.State == ConnectionState.Open)
+//                {
+//                    Console.WriteLine("SQL Connection has beem successfull:");
+//                    Console.ReadLine();
+//                }
+//            }
+//            catch (SqlException ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+//            finally
+//            {
+//                con.Close();
+//            }
+//        }
+//    }
+//}
 
-               }
-            }
-               catch(SqlException ex)
-               {
-                 Console.WriteLine(ex.Message);
-               }
-              finally
-              {
-                con.Close();
-              }
-                   
 
-        }        
-    }
-}
+
+
+
+
+
--- SqlconnectionCheck.cs
+++ SqlconnectionCheck.cs
@@ -9,57 +9,18 @@ namespace SQLConncetionADO
 {
     public class SqlconnectionCheck
     {
+        
+
         public void Conncetion1()
         {
-            Console.WriteLine("Enter Your Employee ID ");
-            int EmpID = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter Your FirstName ");
-            String FirstName = Console.ReadLine();
-
-            Console.WriteLine("Enter Your LastName ");
-            String LastName = Console.ReadLine();
-
-            //Console.WriteLine("Enter Your JoiningDate ");
-            //String JoiningDate = Console.ReadLine(); 
-
-            //Console.WriteLine("Enter Your Department ");
-            //String Department = Console.ReadLine();
-
-            //Console.WriteLine("Enter Your phoneNo ");
-            //float phoneNo = float.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter Your EmailID ");
-            //String EmailID = Console.ReadLine();
-
-            //Console.WriteLine("Enter Your Address ");
-            //String Address = Console.ReadLine();
-
-            //Console.WriteLine("Enter Your State ");
-            //String State = Console.ReadLine();
-
-            //Console.WriteLine("Enter Your City ");
-            //String City = Console.ReadLine();
-
-            //Console.WriteLine("Enter Your Gender ");
-            //Char Gender = Char.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Enter Your BasicPay ");
-            //float BasicPay = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter Your Salary ");
-            float Salary = float.Parse(Console.ReadLine());
-
-
-
             string cs = "Data Source=DESKTOP-C3BQOGP\\SQLEXPRESS;Initial Catalog=SQLConnction;Integrated Security = true;";
             SqlConnection con = new SqlConnection(cs);
-            string query = string.Format("INSERT INTO PayRoll VALUES ({0},'{1}','{2}','{3}')", EmpID, FirstName, LastName, Salary); 
+            string query = string.Format("INSERT INTO PayRoll VALUES(40,'Satish','Mehta','2021-12-31','Busniness Analyst','9852267616','awsthi369@gmail.com','BTM Layout 5st stage','Karanataka','bangalore','M',15000,22000);");
             SqlCommand cmd = new SqlCommand(query, con);
             con.Open();
+           int n = cmd.ExecuteNonQuery();
 
-           cmd.ExecuteNonQuery();
-            //Console.WriteLine("{0} row inserted successfully!",n);
+            Console.WriteLine("{0} row inserted successfully!",n);
             Console.ReadLine();
             con.Close();
             
